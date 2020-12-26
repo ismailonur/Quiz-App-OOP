0bb6d1748788e6faa7d4a3af99a4b9dcbeb3d5f1
ismailonur <ismailonur15@gmail.com>
2020-12-26 11:46:02 +0300

    arayüz yapıldı

--- app.js
+++ app.js
@@ -5,33 +5,33 @@ function Question(text, choices, answer) {
 }
 
 //  Question prototype
-Question.prototype.checkAnswer = function(answer){
+Question.prototype.checkAnswer = function (answer) {
     return this.answer === answer;
 }
 
 // Quiz Constructor
-function Quiz(questions){
+function Quiz(questions) {
     this.questions = questions;
     this.score = 0;
     this.questionIndex = 0;
 }
 
 // Quiz Prototype
-Quiz.prototype.getQuestion = function(){
+Quiz.prototype.getQuestion = function () {
     return this.questions[this.questionIndex];
 }
 
 //Quiz isFinish
 
-Quiz.prototype.isFinish=function(){
+Quiz.prototype.isFinish = function () {
     return this.questions.length === this.questionIndex
 }
 
 // Quiz guess
-Quiz.prototype.guess = function(answer){
+Quiz.prototype.guess = function (answer) {
     var question = this.getQuestion();
 
-    if(question.checkAnswer(answer)){
+    if (question.checkAnswer(answer)) {
         this.score++;
     }
     this.questionIndex++;
@@ -42,22 +42,52 @@ var q1 = new Question("En iyi programlama dili nedir?", ["C#", "JavaScript", "Py
 var q2 = new Question("En popüler programlama dili nedir?", ["C#", "Python", "Visual Basic", "JavaScript"], "JavaScript");
 var q3 = new Question("En modern programlama dili nedir?", ["C#", "JavaScript", "Python", "Asp.net"], "JavaScript");
 
-var questions = [q1,q2,q3]
+var questions = [q1, q2, q3]
 
 
 //Start Quiz
 var quiz = new Quiz(questions);
 
-console.log(quiz.isFinish());
+loadQuestion();
 
-console.log(quiz.getQuestion());
-quiz.guess('JavaScript')
+function loadQuestion() {
+    if (quiz.isFinish()) {
+        showScore();
+    }
+    else {
+        var question = quiz.getQuestion();
+        var choices = question.choices;
 
-console.log(quiz.getQuestion());
-quiz.guess('JavaScript')
+        document.querySelector('#question').textContent = question.text;
 
-console.log(quiz.getQuestion());
-quiz.guess('JavaScript')
+        for (var i = 0; i < choices.length; i++) {
+            var element = document.querySelector('#choice' + i);
+            element.innerHTML = choices[i];
 
-console.log(quiz.score);
-console.log(quiz.isFinish());
+            guess('btn' + i, choices[i]);
+        }
+
+        showProgress()
+    }
+}
+
+function guess(id, guess) {
+    var btn = document.getElementById(id);
+    btn.onclick = function () {
+        quiz.guess(guess);
+        loadQuestion();
+    }
+}
+
+
+function showScore() {
+    var html = `<h2>Score</h2><h4>${quiz.score}</h4>`;
+
+    document.querySelector('.card-body').innerHTML = html;
+}
+
+function showProgress() {
+    var totalQuestion = quiz.questions.length;
+    var questionNumber = quiz.questionIndex + 1;
+    document.querySelector('#progress').innerHTML = "Question " + questionNumber + " of " + totalQuestion;
+}
